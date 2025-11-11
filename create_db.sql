038cf942ef5c0c7fa4a944e26927a31520300bf4
ksenimod <dlupashko@kse.org.ua>
2025-11-11 17:31:17 +0200

    cleaned up create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -2,9 +2,7 @@ CREATE DATABASE shop_dwh;
 
 USE shop_dwh;
 
-DROP TABLE IF EXISTS Customers, Categories, Products, Orders, OrderItems;
-
-CREATE TABLE Customers (
+CREATE TABLE raw.customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
@@ -14,36 +12,36 @@ CREATE TABLE Customers (
     is_premium BOOLEAN
 );
 
-CREATE TABLE Categories (
+CREATE TABLE raw.categories (
     category_id INT PRIMARY KEY AUTO_INCREMENT,
     category_name VARCHAR(50)
 );
 
-CREATE TABLE Products (
+CREATE TABLE raw.products (
     product_id INT PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(100),
     category_id INT,
     price DECIMAL(10, 2),
     stock_quantity INT,
     supplier_id INT,
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
-CREATE TABLE Orders (
+CREATE TABLE raw.orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATETIME,
     total_amount DECIMAL(10, 2),
     status VARCHAR(20),
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES raw.customers(customer_id)
 );
 
-CREATE TABLE OrderItems (
+CREATE TABLE raw.order_items (
     item_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     product_id INT,
     quantity INT,
     unit_price DECIMAL(10, 2),
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    FOREIGN KEY (order_id) REFERENCES raw.orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES raw.products(product_id)
 );
